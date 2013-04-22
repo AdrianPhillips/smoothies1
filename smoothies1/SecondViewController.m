33fd270633119e6c78ac57368b4c0768cb4562dc
Adrian Phillips <adrianphillips@adrians-imac.local.tld>
2013-04-22 09:32:21 -0400

    Initial Commit

---
+++ smoothies1/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  smoothies1
+//
+//  Created by Adrian Phillips on 4/22/13.
+//  Copyright (c) 2013 Adrian Phillips. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ smoothies1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  smoothies1
+//
+//  Created by Adrian Phillips on 4/22/13.
+//  Copyright (c) 2013 Adrian Phillips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
