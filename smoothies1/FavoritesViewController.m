f30d703368e3aa76e7df95cdb6edeb784d5632e3
AdrianPhillips <adrianp31@me.com>
2013-04-27 11:33:29 -0400

    Smoothies1

---
+++ smoothies1/FavoritesViewController.m
@@ -0,0 +1,98 @@
+
+#import "FavoritesViewController.h"
+#import "DataModel.h"
+#import "Recipe.h"
+#import "RecipeDetailsViewController.h"
+
+@implementation FavoritesViewController
+
+@synthesize dataModel;
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+
+	// Set the height of the table view rows
+	self.tableView.rowHeight = 66;
+
+	// Start listening for notifications
+	[[NSNotificationCenter defaultCenter] addObserver:self
+		selector:@selector(favoritesChanged:)
+		name:FavoritesChangedNotification
+		object:nil];
+}
+
+- (void) didReceiveMemoryWarning
+{
+	[super didReceiveMemoryWarning];
+
+	// Stop listening for notifications
+	[[NSNotificationCenter defaultCenter] removeObserver:self
+		name:FavoritesChangedNotification
+		object:nil];
+}
+
+- (void)dealloc
+{
+	// Stop listening for notifications
+	[[NSNotificationCenter defaultCenter] removeObserver:self
+		name:FavoritesChangedNotification
+		object:nil];
+
+	[super dealloc];
+}
+
+- (void)favoritesChanged:(NSNotification*)notification
+{
+	[self.tableView reloadData];
+}
+
+#pragma mark -
+#pragma mark UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView*)theTableView numberOfRowsInSection:(NSInteger)section
+{
+	// Return the number of favorites
+	return [self.dataModel favoritesCount];
+}
+
+- (UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath
+{
+	static NSString* CellIdentifier = @"RecipeCellIdentifier";
+
+	// Obtain a table view cell for this row
+	UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+	if (cell == nil)
+		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+
+	// Get the favorite Recipe from the list
+	Recipe* recipe = [self.dataModel.sortedFavorites objectAtIndex:indexPath.row]; 
+
+	// Put the Recipe data into the cell
+	cell.textLabel.text = recipe.name;
+	cell.imageView.image = recipe.image;
+
+	return cell;
+}
+
+#pragma mark -
+#pragma mark UITableViewDelegate
+
+- (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath
+{
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    // Create the Recipe Details screen
+	RecipeDetailsViewController* controller = [[RecipeDetailsViewController alloc] init];
+	controller.dataModel = self.dataModel;
+    
+	// Tell the Recipe Details screen which Recipe it should display
+	Recipe* recipe = [self.dataModel recipeAtIndex:indexPath.row];
+	controller.recipe = recipe;
+    
+	// Show the Recipe Details screen
+	[self.navigationController pushViewController:controller animated:YES];
+	[controller release];
+}
+
+@end
